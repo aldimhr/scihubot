aef0886cd2c2ca57e87d6fb52946682771563f2a
Aldimhr <dirikuin16@gmail.com>
2022-06-25 14:29:04 +0700

    fixing error

--- app.js
+++ app.js
@@ -138,7 +138,10 @@ bot.use(async (ctx, next) => {
     // bot has been deleted by user
     if (ctx.update.my_chat_member) {
       adminChatId.forEach(async (item) => {
-        ctx.telegram.sendMessage(item, `${responseMessages.delete} ${ctx.my_chat_member.chat.id}`);
+        ctx.telegram.sendMessage(
+          item,
+          `${responseMessages.delete} ${ctx.update.my_chat_member.chat.id}`
+        );
       });
 
       err = true;
