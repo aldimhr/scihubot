94dcd9e864e3b0627e7a3d4c1a0de2d1b8a7829e
Aldimhr <dirikuin16@gmail.com>
2022-09-08 19:21:42 +0700

    fix: netlify functions test

--- netlify/functions/bot/bot.js
+++ netlify/functions/bot/bot.js
@@ -12,8 +12,6 @@ bot.start((ctx) => {
   }
 });
 
-bot.launch();
-
 // AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
 exports.handler = async (event) => {
   try {
